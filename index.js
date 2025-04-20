fc6a9c6d403a3a93097f0d531e37f5d1ed0f7bd0
Jorai33 <47177361+Jorai33@users.noreply.github.com>
2025-04-20 18:45:03 +0200

    feat: write the pseudocode for the playRound function

--- index.js
+++ index.js
@@ -19,8 +19,6 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-console.log(getComputerChoice());
-
 // Create a function getHumanChoice
 function getHumanChoice() {
   // Prompt "Rock, paper or scissors ?"
@@ -31,4 +29,40 @@ function getHumanChoice() {
   return humanChoice;
 }
 
+// Create a function playRound
+// Pass it 2 parameters: "humanChoice" and "computerChoice"
+// Lowercase both parameter values
+// Compare values
+
+// If humanChoice equals computerChoice
+//  Write "It's a tie, try again"
+
+// If humanChoice equals "rock"
+//  If computerChoice equals "scissors"
+//    Write "You win! Rock beats scissors"
+//    Increment humanScore
+//  Else write "You lose! Paper beats rock"
+//    Write "You lose! Paper beats rock"
+//    Increment computerScore
+
+// Else if humanChoice equals "paper"
+//  If computerChoice equals "rock"
+//    Write "You win! Paper beats rock"
+//    Increment humanScore
+//  Else
+//    Write "You lose! Scissors beats paper"
+//    Increment computerScore
+
+// Else
+//  If computerChoice equals "paper"
+//    Write "You win! scissors beats paper"
+//    Increment humanScore
+//  Else
+//    Write "You lose! Rock beats scissors"
+//    Increment computerScore
+
+console.log(getComputerChoice());
 console.log(getHumanChoice());
+
+let humanScore = 0;
+let computerScore = 0;
