ae852707cb33ddfec5c77479c05b7da04ff9b282
Jorai33 <47177361+Jorai33@users.noreply.github.com>
2025-04-20 17:35:20 +0200

    feat: write the pseudocode to get computer choice

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello world");
+/*
+Create a function named getComputerChoice
+Get a random number between 1 and 3
+If random number equals 1, then computers'choice equals "rock"
+Else if random number equals 2, then computers'choice equals "paper"
+Else computers'choice equals "scissors"
+Returns computer's choice
+*/
