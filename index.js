f606255ce98b8d73bef8d1e06d74c016d73f43fd
Jorai33 <47177361+Jorai33@users.noreply.github.com>
2025-04-21 12:29:55 +0200

    refactor: clean the code and remove useless comments

--- index.js
+++ index.js
@@ -1,95 +1,56 @@
-// Create a function named getComputerChoice
 function getComputerChoice() {
-  // Get a random number between 1 and 3
   const randomNumber = Math.floor(Math.random() * 3) + 1;
-  let computerChoice;
 
-  // If random number equals 1, then computers'choice equals "rock"
-  // Else if random number equals 2, then computers'choice equals "paper"
-  // Else computers'choice equals "scissors"
   if (randomNumber === 1) {
-    computerChoice = "rock";
+    return "rock";
   } else if (randomNumber === 2) {
-    computerChoice = "paper";
+    return "paper";
   } else {
-    computerChoice = "scissors";
+    return "scissors";
   }
-
-  // Returns computer's choice
-  return computerChoice;
 }
 
-// Create a function getHumanChoice
 function getHumanChoice() {
-  // Prompt "Rock, paper or scissors ?"
-  // Assign prompt value to human's choice
-  let humanChoice = prompt("Rock, paper or scissors ?");
-
-  // Return human's choice
-  return humanChoice;
+  return prompt("Rock, paper or scissors ?");
 }
 
 function playGame() {
   let humanScore = 0;
   let computerScore = 0;
 
-  // Create a function playRound
-  // Pass it 2 parameters: "humanChoice" and "computerChoice"
   function playRound(humanChoice, computerChoice) {
-    // Lowercase both parameter values
     const fmtHumanChoice = humanChoice.toLowerCase();
     const fmtComputerChoice = computerChoice.toLowerCase();
     let scoreMessage;
 
-    // Compare values
     switch (fmtHumanChoice) {
-      // If humanChoice equals computerChoice
       case fmtComputerChoice:
-        //  Write "It's a tie, try again"
         scoreMessage = "It's a tie, try again";
         break;
-      // If humanChoice equals "rock"
       case "rock":
-        //  If computerChoice equals "scissors"
         if (fmtComputerChoice === "scissors") {
-          //    Write "You win! Rock beats scissors"
           scoreMessage = "You win! Rock beats scissors";
-          //    Increment humanScore
           humanScore++;
         } else {
-          //    Write "You lose! Paper beats rock"
           scoreMessage = "You lose! Paper beats rock";
-          //    Increment computerScore
           computerScore++;
         }
         break;
-      // Else if humanChoice equals "paper"
       case "paper":
-        //  If fmtComputerChoice equals "rock"
         if (fmtComputerChoice === "rock") {
-          //    Write "You win! Paper beats rock"
           scoreMessage = "You win! Paper beats rock";
-          //    Increment humanScore
           humanScore++;
         } else {
-          //    Write "You lose! Scissors beats paper"
           scoreMessage = "You lose! Scissors beats paper";
-          //    Increment computerScore
           computerScore++;
         }
         break;
-      // Else if humanChoice equals "scissors"
       case "scissors":
-        //  If fmtComputerChoice equals "paper"
         if (fmtComputerChoice === "paper") {
-          //    Write "You win! scissors beats paper"
           scoreMessage = "You win! scissors beats paper";
-          //    Increment humanScore
           humanScore++;
         } else {
-          //    Write "You lose! Rock beats scissors"
           scoreMessage = "You lose! Rock beats scissors";
-          //    Increment computerScore
           computerScore++;
         }
     }
